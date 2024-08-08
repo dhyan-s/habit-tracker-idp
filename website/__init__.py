94fdd1d42e68faa0cd63984045c2c8ccc7cd231e
S. Dhyanesh <dhyan130709@gmail.com>
2024-08-08 21:47:28 +0530

    feat!: working on login and signup functionality

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,6 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import os
 from .homepage import home
-from .auth import login, sign_up, forgot_pwd
 from .todolist import todolist
 
 db = SQLAlchemy()
@@ -18,17 +17,19 @@ def create_database(app):
             print("Created Database!")        
             
 def register_blueprints(app):
+    from .auth import login_bp, signup_bp, forgotpwd_bp, logout_bp
+    
     app.register_blueprint(home, url_prefix="/")
-    app.register_blueprint(login, url_prefix="/login/")
-    app.register_blueprint(sign_up, url_prefix="/signup/")
+    app.register_blueprint(login_bp, url_prefix="/login/")
+    app.register_blueprint(signup_bp, url_prefix="/signup/")
     app.register_blueprint(todolist, url_prefix="/todolist/")
-    app.register_blueprint(forgot_pwd, url_prefix="/fp/")
+    app.register_blueprint(forgotpwd_bp, url_prefix="/fp/")
+    app.register_blueprint(logout_bp, url_prefix="/logout/")
     
 def create_login_manager(app):
     from .models import User
-    
     login_manager = LoginManager()
-    login_manager.login_view = 'login.loginpage'
+    login_manager.login_view = 'login.login'
     login_manager.init_app(app)
     
     @login_manager.user_loader
@@ -39,8 +40,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "sjkdfj283472983;sfd3{D8D8{{SDLKJ4LK4L4K4L4KL4L4KK44L}"
 
-    create_database(app)
-    register_blueprints(app)
     create_login_manager(app)
+    register_blueprints(app)
+    create_database(app)
     
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,69 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, login_user, logout_user
 
 # TODO: put all into one blueprint, auth
-login = Blueprint("login", __name__)
-sign_up = Blueprint("sign_up", __name__)
-forgot_pwd = Blueprint("forgot_pwd", __name__)
+login_bp = Blueprint("login", __name__)
+signup_bp = Blueprint("sign_up", __name__)
+forgotpwd_bp = Blueprint("forgot_pwd", __name__)
+logout_bp = Blueprint("logout", __name__)
 
 
-@login.route('/')
-def loginpage():
+@login_bp.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        if user := User.query.filter_by(email=email).first(): # := is assignment with check
+            print('here')
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                flash("Logged in successfully!", category='success')
+                return redirect(url_for('home.homepage'))
+            else:
+                flash("Incorrect password. Try again!", category="error")
+        else:
+            flash("Email does not exist", category="error")
     return render_template("login.html")
 
-@sign_up.route('/')
-def signup_page():
+@signup_bp.route('/', methods=['GET', 'POST'])
+def sign_up():
+    from . import db    
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstname')
+        password1 = request.form.get('psw')
+        password2 = request.form.get('psw-repeat')
+        print('here')
+        
+        if user := User.query.filter_by(email=email).first():
+            flash("Email already exists.", category="error")
+        elif len(email) < 4:
+            flash("Email must be greater than 4 characters.", category="error")
+        elif len(first_name) < 2:
+            flash("First name must be greater than 1 character.", category="error")
+        elif password1 != password2:
+            flash("Passwords don\'t match.", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
+        else:
+            new_user = User(email=email, firstname=first_name, password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("Account created!", category="success")
+            return redirect(url_for('home.homepage'))
     return render_template("sign_up.html")
 
-@forgot_pwd.route('/')
-def fp_page():
+@forgotpwd_bp.route('/')
+def forgot_pwd():
     return render_template("forgot_pwd.html")
+
+@logout_bp.route("/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login.login'))
